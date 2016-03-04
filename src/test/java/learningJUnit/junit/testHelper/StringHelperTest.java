cfe30e8b10ba20de0c912e3c44e8e097c8f0ddc8
solo2002 <jiht1999@gmail.com>
2016-03-04 11:37:32 -0600

    refactor

--- src/test/java/learningJUnit/junit/testHelper/StringHelperTest.java
+++ src/test/java/learningJUnit/junit/testHelper/StringHelperTest.java
@@ -6,13 +6,24 @@ import learningJUnit.junit.helper.StringHelper;
 
 public class StringHelperTest {
 
+	StringHelper helper = new StringHelper();
 	@Test
-	public void test() {
-		StringHelper helper = new StringHelper();
+	//test function should be defined as "void" and "public"
+	//Each function handles with one test
+	public void testTruncateAInFirst2Positions_AinFirst2Positions() {
 		//AACD => CD ACD => CD CDEF => CDEF CDAA => CDAA
 		assertEquals("CD", helper.truncateAInFirst2Positions("AACD"));
 		//expected, actual
+		
+	}
+	@Test
+	public void testTruncateAInFirst2Positions_AinFirstPosition()
+	{
 		assertEquals("CD", helper.truncateAInFirst2Positions("ACD"));
 	}
-
+	@Test
+	public void testTruncateAInFirst2Positions_NoAinFirstPosition()
+	{//you also can highlight one function and use "run as JUnit test" to test a single test
+		assertEquals("CDEF", helper.truncateAInFirst2Positions("CDEF"));
+	}
 }
